e4e012616e83cf4bc8e4a5b116830c8276da2723
Jeffrey Liew <jeffreyliew95@gmail.com>
2020-06-14 14:24:17 +0200

    express setup

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require("express");
+
+const app = express();
+
+// middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => console.log(`Server running on port ${port}`));
