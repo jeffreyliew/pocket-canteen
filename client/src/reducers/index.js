d2832d3153bd1f3ad36fc84a6e757c32875dcef1
Jeffrey Liew <jeffreyliew95@gmail.com>
2020-06-24 16:01:40 +0200

    frontend redux add canteenReducer

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from "redux";
 import authReducer from "./authReducer";
+import canteenReducer from "./canteenReducer";
 import errorReducer from "./errorReducer";
 
 export default combineReducers({
   auth: authReducer,
+  canteen: canteenReducer,
   errors: errorReducer,
 });
