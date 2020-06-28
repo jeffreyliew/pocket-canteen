caf2634011abd79458d93ec60739c821ba68b89f
Jeffrey Liew <jeffreyliew95@gmail.com>
2020-06-28 22:15:34 +0200

    add fields

--- models/User.js
+++ models/User.js
@@ -18,6 +18,56 @@ const UserSchema = new Schema({
     type: Date,
     default: Date.now,
   },
+  favouriteCanteens: [
+    {
+      canteen: {
+        type: Schema.Types.ObjectId,
+        ref: "canteen",
+      },
+    },
+  ],
+  favouriteMeals: [
+    {
+      canteen: {
+        type: Schema.Types.ObjectId,
+        ref: "canteen",
+      },
+      _id: {
+        type: Number,
+        required: true,
+      },
+      id: {
+        type: Number,
+        required: true,
+      },
+      category: {
+        type: String,
+        required: true,
+      },
+      name: {
+        type: String,
+        required: true,
+      },
+      notes: {
+        type: [String],
+        required: true,
+      },
+      prices: {
+        students: {
+          type: Number,
+          required: true,
+        },
+        employees: {
+          type: Number,
+          required: true,
+        },
+        others: {
+          type: Number,
+          required: true,
+        },
+      },
+    },
+  ],
 });
 
 module.exports = mongoose.model("users", UserSchema);
