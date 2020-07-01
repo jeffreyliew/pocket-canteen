28c24e8e96b79304b399faec6964a68ba1862647
Jeffrey Liew <jeffreyliew95@gmail.com>
2020-07-01 14:46:38 +0200

    deploy

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const axios = require("axios");
 const passport = require("passport");
+const path = require("path");
 
 const isEmpty = require("./validation/is-empty");
 
@@ -88,6 +89,16 @@ require("./config/passport")(passport);
 // routes
 app.use("/api/users", users);
 app.use("/api/canteen", canteen);
+
+// serve static assets if in production
+if (process.env.NODE_ENV === "production") {
+  // set static folder
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 
 const port = process.env.PORT || 5000;
 
