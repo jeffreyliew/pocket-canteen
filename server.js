13115815f6f77c5e813f956811aca0ecd08867c7
Jeffrey Liew <jeffreyliew95@gmail.com>
2020-07-02 22:40:17 +0200

    test

--- server.js
+++ server.js
@@ -102,7 +102,7 @@ if (process.env.NODE_ENV === "production") {
 
 // redirect http to https
 app.use((req, res, next) => {
-  req.secure() ? next() : res.redirect("https://" + req.headers.host + req.url);
+  req.secure ? next() : res.redirect("https://" + req.headers.host + req.url);
 });
 
 const port = process.env.PORT || 5000;
