baf42ef501b69d97f566072132a585084eee3225
Jeffrey Liew <jeffreyliew95@gmail.com>
2020-09-14 21:11:48 +0200

    frontend add leaflet map

---
+++ client/src/components/canteens/CanteenMap.js
@@ -0,0 +1,133 @@
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+
+class CanteenMap extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      location: {
+        lat: 51.1657,
+        long: 10.4515,
+      },
+      map: null,
+      canteenLayerGroup: null,
+    };
+  }
+
+  componentDidMount() {
+    // get user location
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition((position) => {
+        const { latitude, longitude } = position.coords;
+
+        this.setState({
+          location: {
+            lat: latitude,
+            long: longitude,
+          },
+        });
+      });
+    }
+
+    // default map setup
+    const { location } = this.state;
+    const { L } = window;
+
+    let map;
+    let canteenMarkers;
+    let canteenLayerGroup;
+
+    // initialize map
+    map = L.map("map").setView([location.lat, location.long], 6);
+
+    L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+      attribution:
+        '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+    }).addTo(map);
+
+    // create canteen markers
+    canteenMarkers = this.props.canteens.map((canteen) => {
+      const { id, name, coordinates } = canteen;
+
+      return L.marker([coordinates.lat, coordinates.long]).bindPopup(
+        `<a href="/canteen/${id}">${name}</a>`
+      );
+    });
+
+    // create canteen layer group
+    canteenLayerGroup = L.layerGroup([...canteenMarkers]);
+
+    // set map and canteen layer group
+    this.setState({
+      map: map,
+      canteenLayerGroup: canteenLayerGroup,
+    });
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    const { L } = window;
+    const {
+      map,
+      canteenLayerGroup,
+      location: { lat, long },
+    } = this.state;
+
+    // set location marker
+    if (prevState.location.lat !== this.state.location.lat) {
+      map.setView([lat, long], 12);
+      L.marker([lat, long])
+        .bindTooltip("Your location", { direction: "top", permanent: true })
+        .addTo(map);
+    }
+
+    // update canteenLayerGroup
+    if (prevProps.canteens !== this.props.canteens) {
+      let canteenMarkers;
+      let newCanteenLayerGroup;
+
+      // clear current canteen layer group
+      canteenLayerGroup.clearLayers();
+
+      // create new canteen markers
+      canteenMarkers = this.props.canteens.map((canteen) => {
+        const { id, name, coordinates } = canteen;
+
+        return L.marker([coordinates.lat, coordinates.long]).bindPopup(
+          `<a href="/canteen/${id}">${name}</a>`
+        );
+      });
+
+      // create new canteen layer group
+      newCanteenLayerGroup = L.layerGroup([...canteenMarkers]);
+
+      // set new canteen layer group
+      this.setState({ canteenLayerGroup: newCanteenLayerGroup });
+    }
+  }
+
+  render() {
+    const { map } = this.state;
+
+    if (map) {
+      // add markers
+      const { canteenLayerGroup } = this.state;
+
+      canteenLayerGroup.addTo(map);
+    }
+
+    return (
+      <div className="row">
+        <div className="col-md-12">
+          <div id="map" style={{ height: "600px" }} />
+        </div>
+      </div>
+    );
+  }
+}
+
+CanteenMap.propTypes = {
+  canteens: PropTypes.array.isRequired,
+};
+
+export default CanteenMap;
--- client/src/components/canteens/Canteens.js
+++ client/src/components/canteens/Canteens.js
@@ -9,15 +9,18 @@ import {
 
 import Spinner from "../common/Spinner";
 import CanteenItem from "./CanteenItem";
+import CanteenMap from "./CanteenMap";
 
 class Canteens extends Component {
   constructor(props) {
     super(props);
     this.state = {
       search: "",
+      hideMap: true,
     };
 
     this.onChange = this.onChange.bind(this);
+    this.toggleMap = this.toggleMap.bind(this);
   }
 
   componentDidMount() {
@@ -44,8 +47,13 @@ class Canteens extends Component {
     this.setState({ [e.target.name]: e.target.value });
   }
 
+  toggleMap() {
+    this.setState({ hideMap: !this.state.hideMap });
+  }
+
   render() {
     const { loading, canteens } = this.props.canteen;
+    const { hideMap } = this.state;
     let canteensContent;
 
     if (loading) {
@@ -103,22 +111,39 @@ class Canteens extends Component {
               </p>
               <hr />
               <div className="row">
-                <div className="col-md-6 m-auto">
-                  <div className="input-group my-3">
-                    <input
-                      type="search"
-                      className="form-control"
-                      placeholder="Search canteen"
-                      name="search"
-                      value={this.state.search}
-                      onChange={this.onChange}
+                <div className="col-9 col-sm-10 col-lg-6 offset-lg-3 mr-lg-auto my-3">
+                  {hideMap && (
+                    <div className="input-group">
+                      <input
+                        type="search"
+                        className="form-control"
+                        placeholder="Search canteen"
+                        name="search"
+                        value={this.state.search}
+                        onChange={this.onChange}
+                      />
+                    </div>
+                  )}
+                </div>
+                <div className="col-3 col-sm-2 col-lg-1 my-3">
+                  <button
+                    type="button"
+                    className="btn btn-light w-100"
+                    onClick={this.toggleMap}
+                  >
+                    <i
+                      className={`${
+                        hideMap
+                          ? "fas fa-map-marker-alt fa-lg"
+                          : "fas fa-times fa-lg"
+                      }`}
                     />
-                  </div>
+                  </button>
                 </div>
               </div>
             </div>
           </div>
-          {canteensContent}
+          {hideMap ? canteensContent : <CanteenMap canteens={canteens} />}
         </div>
       </div>
     );
