c9f6b02ea2ccf31b1c31624c3d216a35afccce65
Jeffrey Liew <jeffreyliew95@gmail.com>
2020-09-24 23:11:08 +0200

    add home icon

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -138,7 +138,7 @@ class Navbar extends Component {
       <nav className="navbar sticky-top navbar-dark bg-dark">
         <div className="container">
           <Link className="navbar-brand" to="/">
-            Home
+            <i class="fas fa-utensils fa-lg" />
           </Link>
 
           {this.props.location.pathname !== "/" && (
